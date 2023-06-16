9bdd91f0b6f6c7fca7b86b0b0111f218b048e463
Ruben0_0 <r.tartwijk@gmail.com>
2023-06-16 20:20:38 +0200

    Adjust filepath for test run

--- Workstation/Test runs.py
+++ Workstation/Test runs.py
@@ -3,8 +3,7 @@ from Synthetic_Sequencer import synthseq as seq, noisify as noise
 from Burgess_Model import main as burg
 
 # TEST RUN 1; F = 4:
-filepath1 = 'D:\AESB\AESB3\BEP\Figures\Burgess Model'
-filepath2 = 'D:\AESB\AESB3\BEP\Figures\Synthetic Sequences'
+filepath = 'D:\AESB\AESB3\BEP\Figures'
 ## DEFINE PARASEQUENCE:
 para_depths = [0, 2.5, 7, 10, 13]
 para_lithologies = ['SST', 'SHSST', 'SLT', 'SH']
@@ -18,8 +17,8 @@ res = 0.25
 n = 5
 ## Create synthetic sequence:
 x, y, derivatives, bounds, dictionaries = seq.sequencer(para_depths, para_lithologies, layout, res, n,
-                                                        alpha=0.4, beta=0.2, filepath=filepath2)
+                                                        alpha=0.4, beta=0.2, filepath=filepath)
 depths, lithologies = noise.gaussian_noise(x, y, derivatives, bounds, dictionaries, layout, res, gamma=0.5,
-                                           filepath=filepath2)
+                                           filepath=filepath)
 ## Run Burgess model:
-result1, result2 = burg.main(depths, lithologies, layout, res, n, filepath1)
+result1, result2 = burg.main(depths, lithologies, layout, res, n, filepath)
